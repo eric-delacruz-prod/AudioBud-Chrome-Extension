75243944dcff2a60be907902fb77213f0b8b414b
Fan <f994y556@ku.edu>
2019-04-08 18:48:43 -0500

    popup.js made

--- scripts/popup.js
+++ scripts/popup.js
@@ -31,6 +31,32 @@ port.onMessage.addListener(function(msg) {
 
   canvasCtx.fillStyle = 'rgb(242, 242, 242)';
   canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-  //canvasCtx.lineWidth = 2;
-  //canvasCtx.beginPath();
+  canvasCtx.lineWidth = 2;
+  canvasCtx.beginPath();
+  var sliceWoidth = WIDTH * 1.0 / bufferLength;
+  var x = 0;
+
+  for(var i=0 ; i < bufferLength; i++)
+  {
+    var data = dataArray[i];
+    var v =data / 128.0;
+    var y = v * HEIGHT/2;
+    
+    var r = data + 120;
+    var g = 255- data;
+    var b = data / 3;
+
+    canvasCtx.StokeStyle =  'rgb(' + r +' , ' + g + ', ' + b + ')';
+    if(i === 0)
+    {
+      canvasCtx.moveTo(x, y);
+    }
+    else{
+      canvasCtx.lineTo(x,y);
+    }
+    x += sliceWidth;
+  
+  }
+  canvasCtx.lineTo(canvas.width, canvas.height/2);
+  canvasCtx.stroke();
 })
