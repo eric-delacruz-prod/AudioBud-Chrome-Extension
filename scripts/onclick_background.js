9614e88c60f8e5f6ad824558ff23def34d44fd52
Eric Seals <ericseals@ku.edu>
2019-04-09 00:24:55 -0500

    Documentation, hopefully this reads better

--- scripts/onclick_background.js
+++ scripts/onclick_background.js
@@ -1,36 +1,72 @@
+//We use the Web Audio API
+//Here is all the documentation needed
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API
+
+
+
 var background = {
   init: function() {
-    //chrome.browserAction.onClicked.addListener(background.onClick);
+    //Use the chrome.runtime API
     chrome.runtime.onMessage.addListener(background.onMessage);
     chrome.runtime.onConnect.addListener(background.onConnect);
   },
   onConnect: function(port) {
-        var audioCtx = new AudioContext();
-        chrome.tabCapture.capture({
+
+        //Creates an AudioContext object representing an audio-processing graph
+        var audioContext = new AudioContext();
+        //First parameter of .capture specifies what type of
+        //information we want from the capture. Here, we want audio.
+        //Second is callback
+        chrome.tabCapture.capture(
+          {
           audio : true,
           video : false
-        }, function(stream) {
-          console.log("fjfjasldkjfasejdf");
-          var audioSourceNode = audioCtx.createMediaStreamSource(stream);
-          var analyserNode = audioCtx.createAnalyser();
+          },
+          function(stream) {
+console.log("fjfjasldkjfasejdf");
+          //stream is a 'MediaStream' that is our audio source
+          var audioSourceNode = audioContext.createMediaStreamSource(stream);
+          //Creates an analyserNode (hence its name)
+          var analyserNode = audioContext.createAnalyser();
+
           audioSourceNode.connect(analyserNode);
-          analyserNode.connect(audioCtx.destination);
+          //destination is the final destination of the audio. So
+          //we connect because it only makes sense to display the
+          //sound which actually reaches the users ears.
+          analyserNode.connect(audioContext.destination);
 
-          analyserNode.fftSize = 1024;
-          console.log("ayylmao");
+
+          //fftSize:=Fast Fourier Transform
+          //Basically specifies the resolution
+          //Power of 2, spans 32->32768
+          analyserNode.fftSize = 16384;
+console.log("ayylmao");
+          //this buffer is always half the fftSize
+          //This is to do with FFT definition, don't adjust here.
           var buffer = analyserNode.frequencyBinCount;
+          //Data array of the buffer
+          //This is our information we are to display
           var data = new Uint8Array(buffer);
-          console.log("wat");
+console.log("wat");
 
+          //We draw an oscilloscope reading of the audio
           function draw() {
             analyserNode.getByteTimeDomainData(data);
+            //This line sends the data to popup.js
+            //The data "enters" popup.js at this line
+            // port.onMessage.addListener(function(message)
             port.postMessage({data: data, bufferLength: buffer});
           };
 
+          //Determine how often we want to redraw the function
+          //Second parameter (in ms) determines the interval.
+          //1000 would be 1 second
           var interval = setInterval(function(){ draw() }, 1000 / 30);
+
           port.onDisconnect.addListener(function() {
+            //close everything down so we can be good lil programmers
             clearInterval(interval);
-            audioCtx.close();
+            audioContext.close();
             stream.getAudioTracks()[0].stop();
           });
         })
--- scripts/popup.js
+++ scripts/popup.js
@@ -14,34 +14,51 @@ act.onclick = function() {
   console.log(tabs);
   chrome.runtime.sendMessage({message:"start",data:tabs[0]});
 })*/
+
+//Uses the canvas API
+//Pretty straightforward, makes drawing to screen simple
+//https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+
 console.log("aaaaaaaaaaaah");
 
+//'visualizer' is the canvas specified in popup.html
 canvas = document.getElementById('visualizer');
-canvasCtx = canvas.getContext("2d");
+//Obvi we are rendering 2d
+canvasContext = canvas.getContext("2d");
 
 var WIDTH = 500;
 var HEIGHT = 500;
 console.log("aaaaaaaaaaaah");
+//This line will connect to onclick_background.js
 var port = chrome.runtime.connect();
+
+//Sends the message 'start' to onclick_background
+//Message doesn't matter, we just want the message to go through.
 port.postMessage({action: 'start'});
 
 port.onMessage.addListener(function(message) {
+  //from onclick_background
   var dataArr = message.data;
   var bufferL = message.bufferLength;
 
-  canvasCtx.fillStyle = 'rgb(50, 50, 70)';
-  canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-  canvasCtx.lineWidth = 1;
-  canvasCtx.beginPath();
+  canvasContext.fillStyle = 'rgb(50, 50, 70)';
+  canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
+  canvasContext.lineWidth = 1;
 
+  //We draw the canvas like a long brush stroke.
+  //This let's the canvas know we are ready to draw.
+  canvasContext.beginPath();
+
+  //thiccness of our line
   var LineWidth = WIDTH * 1.0 / bufferL;
 
   var x = 0;
 
   for(var i=0 ; i < bufferL; i++)
   {
+    //Don't combine the next two lines of code (things break idk)
     var data = dataArr[i];
-   
+    //divide by 128 to get centered vertically
     var v = data / 128.0;
     var y = v * HEIGHT/2;
 
@@ -49,22 +66,22 @@ port.onMessage.addListener(function(message) {
     var g = 185;
     var b = 225;
 
-    //random Linewidth 
+    //random Linewidth
     var numarr=[1,2,3,4,10];
-    canvasCtx.lineWidth = numarr[Math.floor(Math.random()*numarr.length)];  
+    canvasContext.lineWidth = numarr[Math.floor(Math.random()*numarr.length)];
 
-    canvasCtx.strokeStyle =  'rgb(' + r +' , ' + g + ', ' + b + ')';
+    canvasContext.strokeStyle =  'rgb(' + r +' , ' + g + ', ' + b + ')';
     if(i === 0)
     {
-      canvasCtx.moveTo(x, y);
+      canvasContext.moveTo(x, y);
     }
     else{
-      canvasCtx.lineTo(x,y);
-      
+      canvasContext.lineTo(x,y);
+
     }
     x += LineWidth;
-  
+
   }
-  canvasCtx.lineTo(canvas.width, canvas.height/2);
-  canvasCtx.stroke();
+  canvasContext.lineTo(canvas.width, canvas.height/2);
+  canvasContext.stroke();
 })
