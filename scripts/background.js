14520611cfd47cf8cf20bf2c6bbaade11d9d2c08
Evan Trout <fishyevan@gmail.com>
2019-04-30 14:53:35 -0500

    Add second test

--- scripts/background.js
+++ scripts/background.js
@@ -101,7 +101,7 @@ const test1 = function() {
         video: false
       },
       function (stream) {
-        var selectedFilter    = "nofilter"
+        var selectedFilter    = "allpass"
         var selectedFrequency = 440;
         var selectedQValue    = 1;
         var selectedGain      = -6;
@@ -238,7 +238,19 @@ const test1 = function() {
               isPassed = true;
             }
             printResult(isPassed);
+
+
+            console.log("Test 2: Confirm that an existing filter is used");
+            isPassed = false;
+            if (selectedFilter == 'lowpass' || selectedFilter == 'highpass'
+              || selectedFilter == 'bandpass' || selectedFilter == 'lowshelf'
+              || selectedFilter == 'highshelf' || selectedFilter == 'peaking'
+              || selectedFilter == 'notch' || selectedFilter == 'allpass') {
+              isPassed = true;
+            }
+            printResult(isPassed);
           }
+
           //this sets the range
           //too low and the bars start capping out
           analyserNode.maxDecibels = -20
