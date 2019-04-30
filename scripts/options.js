7bf92b57d8dee110c021648486485c4379e74edb
Eric Seals <ericseals@ku.edu>
2019-04-30 10:02:07 -0500

    did something idk

--- scripts/options.js
+++ scripts/options.js
@@ -3,6 +3,12 @@ let page = document.getElementById('buttonDiv');
 const kFilters = ['lowpass', 'highpass', 'bandpass', 'lowshelf',
                   'highshelf', 'peaking', 'notch', 'allpass'];
 
+//Write default values, otherwise this may crash with infinite float error
+chrome.storage.sync.set({filter: 'allpass'}, function(){});
+chrome.storage.sync.set({frequency: 440}, function(){});
+chrome.storage.sync.set({Q: 1}, function(){});
+chrome.storage.sync.set({Gain: 0}, function(){});
+
 function constructOptions(kButtonColors) {
  for (let item of kButtonColors) {
    let button = document.createElement('button');
@@ -17,60 +23,60 @@ function constructOptions(kButtonColors) {
 }
 
 function constructOptionFreq(){
-  let textbox = document.createElement('input');
-  textbox.type = 'number';
-  textbox.min = 10;
-  textbox.max = 22050;
-  textbox.value = 440;
-  textbox.addEventListener('click', function() {
-    chrome.storage.sync.set({frequency: textbox.value}, function() {
-      console.log('frequency is ' + textbox.value);
+  let textboxFreq = document.createElement('input');
+  textboxFreq.type = 'number';
+  textboxFreq.min = 10;
+  textboxFreq.max = 22050;
+  textboxFreq.value = 440;
+  textboxFreq.addEventListener('click', function() {
+    chrome.storage.sync.set({frequency: textboxFreq.value}, function() {
+      console.log('frequency is ' + textboxFreq.value);
     })
   });
-  textbox.addEventListener('keydown', function() {
-    chrome.storage.sync.set({frequency: textbox.value}, function() {
-      console.log('frequency is ' + textbox.value);
+  textboxFreq.addEventListener('keydown', function() {
+    chrome.storage.sync.set({frequency: textboxFreq.value}, function() {
+      console.log('frequency is ' + textboxFreq.value);
     })
   });
-  document.getElementById('freqForm').appendChild(textbox);
+  document.getElementById('freqForm').appendChild(textboxFreq);
 }
 
 function constructOptionQ(){
-  let textbox = document.createElement('input');
-  textbox.type = 'number';
-  textbox.min = .001;
-  textbox.max = 1000;
-  textbox.value = 1;
-  textbox.addEventListener('click', function() {
-    chrome.storage.sync.set({Q: textbox.value}, function() {
-      console.log('Q is ' + textbox.value);
+  let textboxQ = document.createElement('input');
+  textboxQ.type = 'number';
+  textboxQ.min = .001;
+  textboxQ.max = 1000;
+  textboxQ.value = 1;
+  textboxQ.addEventListener('click', function() {
+    chrome.storage.sync.set({Q: textboxQ.value}, function() {
+      console.log('Q is ' + textboxQ.value);
     })
   });
-  textbox.addEventListener('keydown', function() {
-    chrome.storage.sync.set({Q: textbox.value}, function() {
-      console.log('Q is ' + textbox.value);
+  textboxQ.addEventListener('keydown', function() {
+    chrome.storage.sync.set({Q: textboxQ.value}, function() {
+      console.log('Q is ' + textboxQ.value);
     })
   });
-  document.getElementById('qForm').appendChild(textbox);
+  document.getElementById('qForm').appendChild(textboxQ);
 }
 
 function constructOptionGain(){
-  let textbox = document.createElement('input');
-  textbox.type = 'number';
-  textbox.min = -40;
-  textbox.max = 40;
-  textbox.value = 0;
-  textbox.addEventListener('click', function() {
-    chrome.storage.sync.set({Gain: textbox.value}, function() {
-      console.log('Gain is ' + textbox.value);
+  let textboxGain = document.createElement('input');
+  textboxGain.type = 'number';
+  textboxGain.min = -40;
+  textboxGain.max = 40;
+  textboxGain.value = 0;
+  textboxGain.addEventListener('click', function() {
+    chrome.storage.sync.set({Gain: textboxGain.value}, function() {
+      console.log('Gain is ' + textboxGain.value);
     })
   });
-  textbox.addEventListener('keydown', function() {
-    chrome.storage.sync.set({Gain: textbox.value}, function() {
-      console.log('Gain is ' + textbox.value);
+  textboxGain.addEventListener('keydown', function() {
+    chrome.storage.sync.set({Gain: textboxGain.value}, function() {
+      console.log('Gain is ' + textboxGain.value);
     })
   });
-  document.getElementById('gainForm').appendChild(textbox);
+  document.getElementById('gainForm').appendChild(textboxGain);
 }
 
 constructOptions(kFilters);
